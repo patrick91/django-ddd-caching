20d6c22d88cc3d21274e4ddc8f02e533c1071d8d
Patrick Arminio <patrick.arminio@gmail.com>
2020-11-09 21:24:08 +0000

    Add some stats

--- api/views.py
+++ api/views.py
@@ -1,38 +1,54 @@
-from dataclasses import dataclass, field
+import dataclasses
+from dataclasses import dataclass
 from functools import cached_property
 from typing import List
 
 import aioredis
 from campaigns.domain.repositories import CampaignRepository, EventRepository
+from django.http.request import HttpRequest
 from strawberry.dataloader import DataLoader
 from strawberry.django.views import AsyncGraphQLView as BaseAsyncGraphQLView
+from strawberry.http import GraphQLHTTPResponse
+from strawberry.types.execution import ExecutionResult
 
 
-async def load_events(keys: List[str]):
-    repo = EventRepository()
-
-    print("fetching keys", keys)
-
-    return await repo.get_events_batch(keys)
+@dataclass
+class DataFetchingStats:
+    number_of_sql_calls: int = 0
+    number_of_redis_gets: int = 0
+    number_of_redis_sets: int = 0
 
 
 class Repositories:
-    def __init__(self, redis: aioredis.Redis) -> None:
+    def __init__(
+        self, redis: aioredis.Redis, data_fetching_stats: DataFetchingStats
+    ) -> None:
         self.redis = redis
+        self.data_fetching_stats = data_fetching_stats
 
     @cached_property
     def event_repository(self):
-        return EventRepository()
+        return EventRepository(self.redis, self.data_fetching_stats)
 
     @cached_property
     def campaign_repository(self):
-        return CampaignRepository(self.redis)
+        return CampaignRepository(self.redis, self.data_fetching_stats)
 
 
+@dataclass
 class Loaders:
+    repositories: Repositories
+
+    async def load_events(self, keys: List[str]):
+        repo = self.repositories.event_repository
+
+        print("fetching keys", keys)
+
+        return await repo.get_events_batch(keys)
+
     @cached_property
     def event_loader(self):
-        return DataLoader(load_events)
+        return DataLoader(self.load_events)
 
 
 @dataclass
@@ -40,13 +56,29 @@ class Context:
     redis: aioredis.Redis
 
     repositories: Repositories
-    loaders: Loaders = field(default_factory=Loaders)
+    loaders: Loaders
 
 
 class AsyncGraphQLView(BaseAsyncGraphQLView):
     async def get_context(self, request):
+        self.data_fetching_stats = DataFetchingStats()
+
         redis = await aioredis.create_redis_pool("redis://localhost")
 
         await redis.set("my-key", "value")
 
-        return Context(redis=redis, repositories=Repositories(redis))
+        repositories = Repositories(redis, self.data_fetching_stats)
+        loaders = Loaders(repositories)
+
+        return Context(redis=redis, repositories=repositories, loaders=loaders)
+
+    async def process_result(
+        self, request: HttpRequest, result: ExecutionResult
+    ) -> GraphQLHTTPResponse:
+        result = await super().process_result(request, result)
+
+        result["extensions"] = {
+            "dataFetching": dataclasses.asdict(self.data_fetching_stats)
+        }
+
+        return result
--- campaigns/domain/repositories.py
+++ campaigns/domain/repositories.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
+
 from asyncio.tasks import gather
 import dataclasses
 import json
-from typing import Any, Generic, List, Optional, TypeVar
+from typing import Any, Generic, List, Optional, TypeVar, TYPE_CHECKING
 
 import aioredis
 from asgiref.sync import sync_to_async
@@ -12,18 +14,26 @@ from typing_extensions import Protocol
 
 from .entities import Campaign, Event
 
+if TYPE_CHECKING:
+    from api.views import DataFetchingStats
+
 
 class WithId(Protocol):
     id: Any
 
 
 class BaseRedisRepository:
-    DEFAULT_EXPIRE_IN_SECONDS = 5 * 60
+    DEFAULT_EXPIRE_IN_SECONDS = 5
 
-    def __init__(self, redis: aioredis.Redis) -> None:
+    def __init__(
+        self, redis: aioredis.Redis, data_fetching_stats: DataFetchingStats
+    ) -> None:
         self.redis = redis
+        self.data_fetching_stats = data_fetching_stats
 
     async def _cache_entity(self, entity: WithId):
+        self.data_fetching_stats.number_of_redis_sets += 1
+
         await self.redis.set(
             f"{type(entity).__name__}-{entity.id}",
             json.dumps(dataclasses.asdict(entity)),
@@ -31,6 +41,8 @@ class BaseRedisRepository:
         )
 
     async def _get_cached_entity(self, id: Any, entity_class: Any) -> Optional[Any]:
+        self.data_fetching_stats.number_of_redis_gets += 1
+
         entity = await self.redis.get(f"{entity_class.__name__}-{id}")
 
         if not entity:
@@ -47,6 +59,8 @@ class BaseRedisRepository:
 class CampaignRepository(BaseRedisRepository):
     @sync_to_async
     def _get_campaign_from_db(self, id: str) -> Optional[models.Campaign]:
+        self.data_fetching_stats.number_of_sql_calls += 1
+
         return models.Campaign.objects.filter(id=id).first()
 
     async def get_campaign_by_id(self, id: str) -> Optional[Campaign]:
@@ -68,6 +82,8 @@ class CampaignRepository(BaseRedisRepository):
 
     @sync_to_async
     def _get_campaigns_ids(self, first: int) -> List[Campaign]:
+        self.data_fetching_stats.number_of_sql_calls += 1
+
         return list(models.Campaign.objects.all().values_list("id", flat=True)[:first])
 
     async def get_campaigns(self, first: int) -> List[Campaign]:
@@ -76,15 +92,18 @@ class CampaignRepository(BaseRedisRepository):
         return await gather(*(self.get_campaign_by_id(id) for id in ids))
 
 
-class EventRepository:
+class EventRepository(BaseRedisRepository):
     @sync_to_async
     def get_events_for_campaign(self, campaign_id: str) -> List[Event]:
+        self.data_fetching_stats.number_of_sql_calls += 1
         db_events = models.Event.objects.filter(campaign__id=campaign_id).all()
 
         return [Event(id=e.id, title=e.title) for e in db_events]
 
     @sync_to_async
     def get_event_ids(self, campaign_id: str, first: int) -> List[str]:
+        self.data_fetching_stats.number_of_sql_calls += 1
+
         return list(
             models.Event.objects.filter(campaign__id=campaign_id).values_list(
                 "id", flat=True
@@ -93,4 +112,6 @@ class EventRepository:
 
     @sync_to_async
     def get_events_batch(self, event_ids: List[str]) -> List[List[Event]]:
+        self.data_fetching_stats.number_of_sql_calls += 1
+
         return list(models.Event.objects.filter(id__in=event_ids))
