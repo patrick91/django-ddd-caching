81dc97ba4a36ade2204678c217f50c9c3fcdec0b
Patrick Arminio <patrick.arminio@gmail.com>
2020-11-10 15:19:18 +0000

    Use decorator to get stats

--- campaigns/domain/repositories.py
+++ campaigns/domain/repositories.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from asyncio.tasks import gather
 import dataclasses
 import json
-from typing import Any, Generic, List, Optional, TypeVar, TYPE_CHECKING
+from asyncio.tasks import gather
+from typing import TYPE_CHECKING, Any, Dict, List, Optional
 
 import aioredis
 from asgiref.sync import sync_to_async
@@ -22,6 +22,33 @@ class WithId(Protocol):
     id: Any
 
 
+def increase_sql_queries(fn):
+    def wrap(self, *args, **kwargs):
+        self.data_fetching_stats.number_of_sql_calls += 1
+
+        return fn(self, *args, **kwargs)
+
+    return wrap
+
+
+def increase_redis_sets(fn):
+    def wrap(self, *args, **kwargs):
+        self.data_fetching_stats.number_of_redis_sets += 1
+
+        return fn(self, *args, **kwargs)
+
+    return wrap
+
+
+def increase_redis_gets(fn):
+    def wrap(self, *args, **kwargs):
+        self.data_fetching_stats.number_of_redis_gets += 1
+
+        return fn(self, *args, **kwargs)
+
+    return wrap
+
+
 class BaseRedisRepository:
     DEFAULT_EXPIRE_IN_SECONDS = 5
 
@@ -31,20 +58,15 @@ class BaseRedisRepository:
         self.redis = redis
         self.data_fetching_stats = data_fetching_stats
 
+    @increase_redis_sets
     async def _cache_entity(self, entity: WithId):
-        self.data_fetching_stats.number_of_redis_sets += 1
-
         await self.redis.set(
             f"{type(entity).__name__}-{entity.id}",
             json.dumps(dataclasses.asdict(entity)),
             expire=self.DEFAULT_EXPIRE_IN_SECONDS,
         )
 
-    async def _get_cached_entity(self, id: Any, entity_class: Any) -> Optional[Any]:
-        self.data_fetching_stats.number_of_redis_gets += 1
-
-        entity = await self.redis.get(f"{entity_class.__name__}-{id}")
-
+    def _convert_dict_to_entity(self, entity: str, entity_class: Any) -> Optional[Any]:
         if not entity:
             return None
 
@@ -55,12 +77,45 @@ class BaseRedisRepository:
 
             return None
 
+    @increase_redis_sets
+    async def _cache_entities_batch(self, entities: List[WithId]):
+        entities_dict = {
+            f"{type(entity).__name__}-{entity.id}": json.dumps(
+                dataclasses.asdict(entity)
+            )
+            for entity in entities
+        }
+
+        # TODO: set expiry
+
+        await self.redis.mset(
+            entities_dict,
+            # expire=self.DEFAULT_EXPIRE_IN_SECONDS,
+        )
+
+    @increase_redis_gets
+    async def _get_cached_entity(self, id: Any, entity_class: Any) -> Optional[Any]:
+        entity = await self.redis.get(f"{entity_class.__name__}-{id}")
+
+        return self._convert_dict_to_entity(entity, entity_class)
+
+    @increase_redis_gets
+    async def _get_cached_entities_batch(
+        self, ids: List[Any], entity_class: Any
+    ) -> List[Any]:
+        keys = [f"{entity_class.__name__}-{id}" for id in ids]
+
+        entities = await self.redis.mget(*keys)
+
+        return [
+            self._convert_dict_to_entity(entity, entity_class) for entity in entities
+        ]
+
 
 class CampaignRepository(BaseRedisRepository):
+    @increase_sql_queries
     @sync_to_async
     def _get_campaign_from_db(self, id: str) -> Optional[models.Campaign]:
-        self.data_fetching_stats.number_of_sql_calls += 1
-
         return models.Campaign.objects.filter(id=id).first()
 
     async def get_campaign_by_id(self, id: str) -> Optional[Campaign]:
@@ -80,10 +135,9 @@ class CampaignRepository(BaseRedisRepository):
 
         return campaign_entity
 
+    @increase_sql_queries
     @sync_to_async
     def _get_campaigns_ids(self, first: int) -> List[Campaign]:
-        self.data_fetching_stats.number_of_sql_calls += 1
-
         return list(models.Campaign.objects.all().values_list("id", flat=True)[:first])
 
     async def get_campaigns(self, first: int) -> List[Campaign]:
@@ -93,25 +147,74 @@ class CampaignRepository(BaseRedisRepository):
 
 
 class EventRepository(BaseRedisRepository):
+    @increase_sql_queries
     @sync_to_async
     def get_events_for_campaign(self, campaign_id: str) -> List[Event]:
-        self.data_fetching_stats.number_of_sql_calls += 1
         db_events = models.Event.objects.filter(campaign__id=campaign_id).all()
 
         return [Event(id=e.id, title=e.title) for e in db_events]
 
+    @increase_sql_queries
     @sync_to_async
     def get_event_ids(self, campaign_id: str, first: int) -> List[str]:
-        self.data_fetching_stats.number_of_sql_calls += 1
-
         return list(
             models.Event.objects.filter(campaign__id=campaign_id).values_list(
                 "id", flat=True
             )[:first]
         )
 
+    @increase_sql_queries
     @sync_to_async
-    def get_events_batch(self, event_ids: List[str]) -> List[List[Event]]:
-        self.data_fetching_stats.number_of_sql_calls += 1
+    def _get_event_from_db(self, id: str) -> Optional[models.Event]:
+        return models.Event.objects.filter(id=id).first()
 
-        return list(models.Event.objects.filter(id__in=event_ids))
+    @increase_sql_queries
+    @sync_to_async
+    def _get_events_from_db(self, ids: List[str]) -> List[models.Event]:
+        return list(models.Event.objects.filter(id__in=ids))
+
+    async def get_event_by_id(self, id: str) -> Optional[Event]:
+        event_entity = await self._get_cached_entity(id, Event)
+
+        if event_entity:
+            return event_entity
+
+        db_event = await self._get_event_from_db(id)
+
+        if not db_event:
+            return None
+
+        event_entity = Event(id=str(db_event.id), title=db_event.title)
+
+        await self._cache_entity(event_entity)
+
+        return event_entity
+
+    async def get_events_batch(self, event_ids: List[str]) -> List[Event]:
+        entities = await self._get_cached_entities_batch(event_ids, Event)
+
+        if all(entities):
+            return entities
+
+        entities_dict = {}
+
+        missing_events_ids = []
+
+        for event_id, entity in zip(event_ids, entities):
+            entities_dict[str(event_id)] = entity
+
+            if not entity:
+                missing_events_ids.append(event_id)
+
+        missing_events_db = await self._get_events_from_db(missing_events_ids)
+        missing_entities = []
+
+        for db_event in missing_events_db:
+            entity = Event(id=str(db_event.id), title=db_event.title)
+
+            missing_entities.append(entity)
+            entities_dict[str(db_event.id)] = entity
+
+        await self._cache_entities_batch(missing_entities)
+
+        return list(entities_dict.values())
